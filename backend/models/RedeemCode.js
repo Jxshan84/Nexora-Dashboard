2eed576fff8102ebd2cb69f812f2d326637f2a2c
Jxshan84 <sasukeuchicha46535@gmail.com>
2026-07-06 15:30:35 +0530

    Create RedeemCode.js

---
+++ backend/models/RedeemCode.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const redeemCodeSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    unique: true,
+    required: true,
+    uppercase: true
+  },
+
+  reward: {
+    type: Number,
+    required: true
+  },
+
+  uses: {
+    type: Number,
+    default: 1
+  },
+
+  used: {
+    type: Number,
+    default: 0
+  },
+
+  expiresAt: {
+    type: Date,
+    required: true
+  },
+
+  createdBy: {
+    type: String,
+    required: true
+  }
+
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("RedeemCode", redeemCodeSchema);
