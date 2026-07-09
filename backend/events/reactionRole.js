8fd6d7d7cc31bb73047d28d3ea4cda5bcb32371c
Jxshan84 <sasukeuchicha46535@gmail.com>
2026-07-09 13:09:57 +0530

    Update reactionRole.js

--- backend/events/reactionRole.js
+++ backend/events/reactionRole.js
@@ -2,56 +2,92 @@ const ReactionRole = require("../models/ReactionRole");
 
 module.exports = (client) => {
 
-client.on("messageReactionAdd", async (reaction, user) => {
-
+  client.on("messageReactionAdd", async (reaction, user) => {
     if (user.bot) return;
 
     try {
+      if (reaction.partial) await reaction.fetch();
+      if (reaction.message.partial) await reaction.message.fetch();
 
-        if (reaction.partial) await reaction.fetch();
+      const guild = reaction.message.guild;
+      if (!guild) return;
 
-        const data = await ReactionRole.findOne({
-            guildId: reaction.message.guild.id,
-            messageId: reaction.message.id,
-            emoji: reaction.emoji.toString()
-        });
+      const emoji = reaction.emoji.toString();
 
-        if (!data) return;
+      const data = await ReactionRole.findOne({
+        guildId: guild.id,
+        messageId: reaction.message.id,
+        "roles.emoji": emoji
+      });
 
-        const member = await reaction.message.guild.members.fetch(user.id);
+      if (!data) return;
 
-        await member.roles.add(data.roleId);
+      const roleData = data.roles.find(r => r.emoji === emoji);
+      if (!roleData) return;
+
+      const member = await guild.members.fetch(user.id);
+      const role = guild.roles.cache.get(roleData.roleId);
+
+      if (!role) return;
+
+      if (data.type === "unique") {
+        for (const r of data.roles) {
+          if (r.roleId !== roleData.roleId) {
+            await member.roles.remove(r.roleId).catch(() => {});
+          }
+        }
+      }
+
+      if (data.type === "reversed") {
+        await member.roles.remove(role.id).catch(() => {});
+      } else {
+        await member.roles.add(role.id).catch(() => {});
+      }
 
     } catch (err) {
-        console.error("Reaction Add Error:", err);
+      console.error("Reaction Add Error:", err);
     }
+  });
 
-});
-
-client.on("messageReactionRemove", async (reaction, user) => {
-
+  client.on("messageReactionRemove", async (reaction, user) => {
     if (user.bot) return;
 
     try {
+      if (reaction.partial) await reaction.fetch();
+      if (reaction.message.partial) await reaction.message.fetch();
 
-        if (reaction.partial) await reaction.fetch();
+      const guild = reaction.message.guild;
+      if (!guild) return;
 
-        const data = await ReactionRole.findOne({
-            guildId: reaction.message.guild.id,
-            messageId: reaction.message.id,
-            emoji: reaction.emoji.toString()
-        });
+      const emoji = reaction.emoji.toString();
 
-        if (!data) return;
+      const data = await ReactionRole.findOne({
+        guildId: guild.id,
+        messageId: reaction.message.id,
+        "roles.emoji": emoji
+      });
 
-        const member = await reaction.message.guild.members.fetch(user.id);
+      if (!data) return;
 
-        await member.roles.remove(data.roleId);
+      const roleData = data.roles.find(r => r.emoji === emoji);
+      if (!roleData) return;
+
+      const member = await guild.members.fetch(user.id);
+      const role = guild.roles.cache.get(roleData.roleId);
+
+      if (!role) return;
+
+      if (data.type === "verify") return;
+
+      if (data.type === "reversed") {
+        await member.roles.add(role.id).catch(() => {});
+      } else {
+        await member.roles.remove(role.id).catch(() => {});
+      }
 
     } catch (err) {
-        console.error("Reaction Remove Error:", err);
+      console.error("Reaction Remove Error:", err);
     }
-
-});
+  });
 
 };
